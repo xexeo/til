c294215dc8946c2476345e50ddc36301b81928f2
Geraldo Xexéo <xexeo@cos.ufrj.br>
2020-05-10 22:28:45 -0300

    teste 1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Today I Learneds
 
-Aprendi sobre isso no Weblog do [Simon Willison](https://simonwillison.net/2020/Apr/20/self-rewriting-readme/),
+Aprendi sobre isso no Weblog do [Simon Willison](https://simonwillison.net/2020/Apr/20/self-rewriting-readme/), 
 que eu não tenho a menor ideia de quem é.
 
 Ele diz que [John Branchaud](https://github.com/jbranchaud/til) explica que um TIL é:
@@ -8,6 +8,7 @@ Ele diz que [John Branchaud](https://github.com/jbranchaud/til) explica que um T
 > A collection of concise write-ups on small things I learn day to day across a variety
 > of languages and technologies. These are things that don’t really warrant a full blog post.
 # Index
- * # [til](././til.md)
- * ## [outlines](././latex/outlines.md)
-   - [show slide number and total](././latex/beamer/show_slide_number_and_total.md)
+*[outlines](./latex/outlines.md)
+*[vazio](./latex/vazio.md)
+ * ###beamer
+*[show slide number and total](./latex/beamer/show_slide_number_and_total.md)
--- newtils.py
+++ newtils.py
@@ -1,10 +1,7 @@
 import os
 from glob import glob
 
-
-
-MD_FILE_LIST = (y for x in os.walk(".")
-                for y in glob(os.path.join(x[0], '*.md')))
+MD_FILE_LIST = (y for y in os.walk("."))
 
 with open('README.md', 'w') as f:
     text = []
@@ -12,14 +9,19 @@ with open('README.md', 'w') as f:
         for line in g:
             f.write(line)
     for m_file in MD_FILE_LIST:
-        levels = m_file.count(os.sep)
-        if levels < 3:
-            text.append( ' * ' + '#' * levels + ' [%s](./%s)\n' % (
-                                 m_file.split(os.sep)[-1].replace('.md', '').replace('_',' '),
-                                 m_file.replace(os.sep,'/')))
-        else:
-            text.append('%s[%s](./%s)\n' % (m_file.count(os.sep)*' ' + '- ',
-                     m_file.split(os.sep)[-1].replace('.md', '').replace('_',' '),
-                     m_file.replace(os.sep, '/')))
+        # processa o diretório
+            directory = m_file[0].split(os.sep)
+            files = m_file[2]
+            levels = len(directory)
+            last = directory[-1]
+            if files and files[0].find(".md")!=-1:
+                text.append(' * '+'#' * levels+'%s\n' % last )
+                for file in files:
+                    text.append('*[%s](./%s)\n' %
+                        ( file.replace('.md', '').replace('_',' '),
+                          m_file[0].replace(os.sep, '/')[2:]+"/"+file.replace(os.sep, '/')
+                        )
+                    )
+
     text[0] = "# Index\n"
     f.writelines(text)
